fe3abbf6dc654f5619d89cf13e0b94bbb6e5972b
Jorge López <jlopez@cartolab.es>
2011-03-15 15:52:43 +0100

    Vivienda and Parcela interfaces improved.
    Now several fields are enabled/disabled in accordance
    with the values input in related ones.

--- src/es/udc/cartolab/gvsig/pmf/forms/ParcelaForm.java
+++ src/es/udc/cartolab/gvsig/pmf/forms/ParcelaForm.java
@@ -1,8 +1,13 @@
 package es.udc.cartolab.gvsig.pmf.forms;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 import org.apache.log4j.Logger;
 
@@ -120,7 +125,295 @@ public class ParcelaForm extends AbstractForm
 		return true;
 	}
 
+	private void setCodigo_fcEnabledIfNeeded() {
+		JCheckBox fuente_co = (JCheckBox) formBody.getComponentByName("fuente_co.CHB");
+		JTextField codigo_fc = (JTextField) formBody.getComponentByName("codigo_fc.TF");
+		JButton codigo_fc_bt = (JButton) formBody.getComponentByName("codigo_fc.BT");
+
+		if (fuente_co.isSelected()) {
+			codigo_fc.setEnabled(true);
+			codigo_fc_bt.setEnabled(true);
+		} else {
+			codigo_fc.setEnabled(false);
+			codigo_fc_bt.setEnabled(false);
+		}
+	}
+
+	private void setOt_legal_pEnabledIfNeeded() {
+		JComboBox legal_par = (JComboBox) formBody.getComponentByName("legal_par.CB");
+		JTextField ot_legal_p = (JTextField) formBody.getComponentByName("ot_legal_p.TF");
+
+		if (legal_par.getSelectedItem().equals("Otro")) {
+			ot_legal_p.setEnabled(true);
+		} else {
+			ot_legal_p.setEnabled(false);
+		}
+	}
+
+	private void setD_fue_tanoEnabledIfNeeded() {
+		JComboBox d_fue_tan = (JComboBox) formBody.getComponentByName("d_fue_tan.CB");
+		JTextField d_fue_tano = (JTextField) formBody.getComponentByName("d_fue_tano.TF.REAL");
+
+		if (d_fue_tan.getSelectedItem().equals("Otra")) {
+			d_fue_tano.setEnabled(true);
+		} else {
+			d_fue_tano.setEnabled(false);
+		}
+	}
+
+	private void setD_tan_hueoEnabledIfNeeded() {
+		JComboBox d_tan_hue = (JComboBox) formBody.getComponentByName("d_tan_hue.CB");
+		JTextField d_tan_hueo = (JTextField) formBody.getComponentByName("d_tan_hueo.TF.REAL");
+
+		if (d_tan_hue.getSelectedItem().equals("Otra")) {
+			d_tan_hueo.setEnabled(true);
+		} else {
+			d_tan_hueo.setEnabled(false);
+		}
+	}
+
+	private void setOt_tip_suEnabledIfNeeded() {
+		JComboBox tip_suelo = (JComboBox) formBody.getComponentByName("tip_suelo.CB");
+		JTextField ot_tip_su = (JTextField) formBody.getComponentByName("ot_tip_su.TF");
+
+		if (tip_suelo.getSelectedItem().equals("Otro")) {
+			ot_tip_su.setEnabled(true);
+		} else {
+			ot_tip_su.setEnabled(false);
+		}
+	}
+
+	private void setCercasEnabledIfNeeded() {
+		JCheckBox cerca = (JCheckBox) formBody.getComponentByName("cerca.CHB");
+		JCheckBox b_vivas = (JCheckBox) formBody.getComponentByName("b_vivas.CHB");
+		JCheckBox b_muertas = (JCheckBox) formBody.getComponentByName("b_muertas.CHB");
+		JCheckBox hay_ot_cer = (JCheckBox) formBody.getComponentByName("hay_ot_cer.CHB");
+		JTextField ot_cerca = (JTextField) formBody.getComponentByName("ot_cerca.TF");
+
+		if (cerca.isSelected()) {
+			b_vivas.setEnabled(true);
+			b_muertas.setEnabled(true);
+			hay_ot_cer.setEnabled(true);
+			if (hay_ot_cer.isSelected())
+				ot_cerca.setEnabled(true);
+		} else {
+			b_vivas.setEnabled(false);
+			b_muertas.setEnabled(false);
+			hay_ot_cer.setEnabled(false);
+			ot_cerca.setEnabled(false);
+		}
+	}
+
+	private void setOt_cercaEnabledIfNeeded() {
+		JCheckBox hay_ot_cer = (JCheckBox) formBody.getComponentByName("hay_ot_cer.CHB");
+		JTextField ot_cerca = (JTextField) formBody.getComponentByName("ot_cerca.TF");
+
+		if (hay_ot_cer.isSelected()) {
+			ot_cerca.setEnabled(true);
+		} else {
+			ot_cerca.setEnabled(false);
+		}
+	}
+
+	private void setOtrocanEnabledIfNeeded() {
+		JCheckBox hay_ot_cul = (JCheckBox) formBody.getComponentByName("hay_ot_cul.CHB");
+		JTextField otrocan = (JTextField) formBody.getComponentByName("otrocan.TF");
+
+		if (hay_ot_cul.isSelected()) {
+			otrocan.setEnabled(true);
+		} else {
+			otrocan.setEnabled(false);
+		}
+	}
+
+	private void setOtrocspeEnabledIfNeeded() {
+		JCheckBox hay_ot_sp = (JCheckBox) formBody.getComponentByName("hay_ot_sp.CHB");
+		JTextField otrocspe = (JTextField) formBody.getComponentByName("otrocspe.TF");
+
+		if (hay_ot_sp.isSelected()) {
+			otrocspe.setEnabled(true);
+		} else {
+			otrocspe.setEnabled(false);
+		}
+	}
+
+	private void setC_conseEnabledIfNeeded() {
+		JCheckBox prac_conse = (JCheckBox) formBody.getComponentByName("prac_conse.CHB");
+		JTextField c_conse = (JTextField) formBody.getComponentByName("c_conse.TF");
+
+		if (prac_conse.isSelected()) {
+			c_conse.setEnabled(true);
+		} else {
+			c_conse.setEnabled(false);
+		}
+	}
+
+	private void setC_aborgEnabledIfNeeded() {
+		JCheckBox uso_aborg = (JCheckBox) formBody.getComponentByName("uso_aborg.CHB");
+		JTextField c_aborg = (JTextField) formBody.getComponentByName("c_aborg.TF");
+
+		if (uso_aborg.isSelected()) {
+			c_aborg.setEnabled(true);
+		} else {
+			c_aborg.setEnabled(false);
+		}
+	}
+
+	private void setC_insectEnabledIfNeeded() {
+		JCheckBox insect_org = (JCheckBox) formBody.getComponentByName("insect_org.CHB");
+		JTextField c_insect = (JTextField) formBody.getComponentByName("c_insect.TF");
+
+		if (insect_org.isSelected()) {
+			c_insect.setEnabled(true);
+		} else {
+			c_insect.setEnabled(false);
+		}
+	}
+
+	private void setC_quimEnabledIfNeeded() {
+		JCheckBox uso_quim = (JCheckBox) formBody.getComponentByName("uso_quim.CHB");
+		JTextField c_quim = (JTextField) formBody.getComponentByName("c_quim.TF");
+
+		if (uso_quim.isSelected()) {
+			c_quim.setEnabled(true);
+		} else {
+			c_quim.setEnabled(false);
+		}
+	}
+
 	@Override
 	protected void fillSpecificValues() {
+		setCodigo_fcEnabledIfNeeded();
+		setOt_legal_pEnabledIfNeeded();
+		setD_tan_hueoEnabledIfNeeded();
+		setD_fue_tanoEnabledIfNeeded();
+		setOt_tip_suEnabledIfNeeded();
+		setCercasEnabledIfNeeded();
+		setOt_cercaEnabledIfNeeded();
+		setOtrocanEnabledIfNeeded();
+		setOtrocspeEnabledIfNeeded();
+		setC_quimEnabledIfNeeded();
+		setC_insectEnabledIfNeeded();
+		setC_aborgEnabledIfNeeded();
+		setC_conseEnabledIfNeeded();
+	}
+
+	protected void setListeners() {
+		super.setListeners();
+		JCheckBox fuente_co = (JCheckBox) formBody.getComponentByName("fuente_co.CHB");
+		fuente_co.setActionCommand("codigo_fc");
+		fuente_co.addActionListener(this);
+		JComboBox legal_par = (JComboBox) formBody.getComponentByName("legal_par.CB");
+		legal_par.setActionCommand("ot_legal_p");
+		legal_par.addActionListener(this);
+		JComboBox d_fue_tan = (JComboBox) formBody.getComponentByName("d_fue_tan.CB");
+		d_fue_tan.setActionCommand("d_fue_tano");
+		d_fue_tan.addActionListener(this);
+		JComboBox d_tan_hue = (JComboBox) formBody.getComponentByName("d_tan_hue.CB");
+		d_tan_hue.setActionCommand("d_tan_hueo");
+		d_tan_hue.addActionListener(this);
+		JComboBox tip_suelo = (JComboBox) formBody.getComponentByName("tip_suelo.CB");
+		tip_suelo.setActionCommand("ot_tip_su");
+		tip_suelo.addActionListener(this);
+		JCheckBox cerca = (JCheckBox) formBody.getComponentByName("cerca.CHB");
+		cerca.setActionCommand("cercas");
+		cerca.addActionListener(this);
+		JCheckBox hay_ot_cer = (JCheckBox) formBody.getComponentByName("hay_ot_cer.CHB");
+		hay_ot_cer.setActionCommand("hay_ot_cer");
+		hay_ot_cer.addActionListener(this);
+		JCheckBox hay_ot_cul = (JCheckBox) formBody.getComponentByName("hay_ot_cul.CHB");
+		hay_ot_cul.setActionCommand("hay_ot_cul");
+		hay_ot_cul.addActionListener(this);
+		JCheckBox hay_ot_sp = (JCheckBox) formBody.getComponentByName("hay_ot_sp.CHB");
+		hay_ot_sp.setActionCommand("hay_ot_sp");
+		hay_ot_sp.addActionListener(this);
+		JCheckBox prac_conse = (JCheckBox) formBody.getComponentByName("prac_conse.CHB");
+		prac_conse.setActionCommand("prac_conse");
+		prac_conse.addActionListener(this);
+		JCheckBox uso_aborg = (JCheckBox) formBody.getComponentByName("uso_aborg.CHB");
+		uso_aborg.setActionCommand("uso_aborg");
+		uso_aborg.addActionListener(this);
+		JCheckBox insect_org = (JCheckBox) formBody.getComponentByName("insect_org.CHB");
+		insect_org.setActionCommand("insect_org");
+		insect_org.addActionListener(this);
+		JCheckBox uso_quim = (JCheckBox) formBody.getComponentByName("uso_quim.CHB");
+		uso_quim.setActionCommand("uso_quim");
+		uso_quim.addActionListener(this);
+	}
+
+	@Override
+	protected void removeListeners() {
+		JCheckBox fuente_co = (JCheckBox) formBody.getComponentByName("fuente_co.CHB");
+		fuente_co.removeActionListener(this);
+		JComboBox legal_par = (JComboBox) formBody.getComponentByName("legal_par.CB");
+		legal_par.removeActionListener(this);
+		JComboBox d_fue_tan = (JComboBox) formBody.getComponentByName("d_fue_tan.CB");
+		d_fue_tan.removeActionListener(this);
+		JComboBox d_tan_hue = (JComboBox) formBody.getComponentByName("d_tan_hue.CB");
+		d_tan_hue.removeActionListener(this);
+		JComboBox tip_suelo = (JComboBox) formBody.getComponentByName("tip_suelo.CB");
+		tip_suelo.removeActionListener(this);
+		JCheckBox cerca = (JCheckBox) formBody.getComponentByName("cerca.CHB");
+		cerca.removeActionListener(this);
+		JCheckBox hay_ot_cer = (JCheckBox) formBody.getComponentByName("hay_ot_cer.CHB");
+		hay_ot_cer.removeActionListener(this);
+		JCheckBox hay_ot_cul = (JCheckBox) formBody.getComponentByName("hay_ot_cul.CHB");
+		hay_ot_cul.removeActionListener(this);
+		JCheckBox hay_ot_sp = (JCheckBox) formBody.getComponentByName("hay_ot_sp.CHB");
+		hay_ot_sp.removeActionListener(this);
+		JCheckBox prac_conse = (JCheckBox) formBody.getComponentByName("prac_conse.CHB");
+		prac_conse.removeActionListener(this);
+		JCheckBox uso_aborg = (JCheckBox) formBody.getComponentByName("uso_aborg.CHB");
+		uso_aborg.removeActionListener(this);
+		JCheckBox insect_org = (JCheckBox) formBody.getComponentByName("insect_org.CHB");
+		insect_org.removeActionListener(this);
+		JCheckBox uso_quim = (JCheckBox) formBody.getComponentByName("uso_quim.CHB");
+		uso_quim.removeActionListener(this);
+		super.removeListeners();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		super.actionPerformed(e);
+		String action = e.getActionCommand();
+		if (action.equals("codigo_fc")) {
+			setCodigo_fcEnabledIfNeeded();
+		} else 
+			if (action.equals("ot_legal_p")) {
+				setOt_legal_pEnabledIfNeeded();
+			} else
+				if (action.equals("d_fue_tano")) {
+					setD_fue_tanoEnabledIfNeeded();
+				} else
+					if (action.equals("d_tan_hueo")) {
+						setD_tan_hueoEnabledIfNeeded();
+					} else
+						if (action.equals("ot_tip_su")) {
+							setOt_tip_suEnabledIfNeeded();
+						} else
+							if (action.equals("cercas")) {
+								setCercasEnabledIfNeeded();
+							} else
+								if (action.equals("hay_ot_cer")) {
+									setOt_cercaEnabledIfNeeded();
+								} else
+									if (action.equals("hay_ot_cul")) {
+										setOtrocanEnabledIfNeeded();
+									} else
+										if (action.equals("hay_ot_sp")) {
+											setOtrocspeEnabledIfNeeded();
+										} else
+											if (action.equals("prac_conse")) {
+												setC_conseEnabledIfNeeded();
+											} else
+												if (action.equals("uso_aborg")) {
+													setC_aborgEnabledIfNeeded();
+												} else
+													if (action.equals("insect_org")) {
+														setC_insectEnabledIfNeeded();
+													} else
+														if (action.equals("uso_quim")) {
+															setC_quimEnabledIfNeeded();
+														}
 	}
 }
--- src/es/udc/cartolab/gvsig/pmf/forms/ViviendaForm.java
+++ src/es/udc/cartolab/gvsig/pmf/forms/ViviendaForm.java
@@ -1,8 +1,12 @@
 package es.udc.cartolab.gvsig.pmf.forms;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
 
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 import org.apache.log4j.Logger;
 
@@ -120,7 +124,202 @@ public class ViviendaForm extends AbstractForm
 		return true;
 	}
 
+	private void setOtras_actEnabledIfNeeded() {
+		JCheckBox hay_ot_act = (JCheckBox) formBody.getComponentByName("hay_ot_act.CHB");
+		JTextField otras_act = (JTextField) formBody.getComponentByName("otras_act.TF");
+
+		if (hay_ot_act.isSelected()) {
+			otras_act.setEnabled(true);
+		} else {
+			otras_act.setEnabled(false);
+		}
+	}
+
+	private void setOtros_ingEnabledIfNeeded() {
+		JCheckBox hay_ot_ing = (JCheckBox) formBody.getComponentByName("hay_ot_ing.CHB");
+		JTextField otros_ing = (JTextField) formBody.getComponentByName("otros_ing.TF");
+
+		if (hay_ot_ing.isSelected()) {
+			otros_ing.setEnabled(true);
+		} else {
+			otros_ing.setEnabled(false);
+		}
+	}
+
+	private void setOt_stat_viEnabledIfNeeded() {
+		JComboBox estatus_vi = (JComboBox) formBody.getComponentByName("estatus_vi.CB");
+		JTextField ot_stat_vi = (JTextField) formBody.getComponentByName("ot_stat_vi.TF");
+
+		if (estatus_vi.getSelectedItem().equals("Otro")) {
+			ot_stat_vi.setEnabled(true);
+		} else {
+			ot_stat_vi.setEnabled(false);
+		}
+	}
+
+	private void setPro_vivsexEnabledIfNeeded() {
+		JCheckBox pro_viv = (JCheckBox) formBody.getComponentByName("pro_viv.CHB");
+		JComboBox pro_vivsex = (JComboBox) formBody.getComponentByName("pro_vivsex.CB");
+
+		if (pro_viv.isSelected()) {
+			pro_vivsex.setEnabled(true);
+		} else {
+			pro_vivsex.setEnabled(false);
+		}
+	}
+
+	private void setOt_mat_paEnabledIfNeeded() {
+		JComboBox mat_pared = (JComboBox) formBody.getComponentByName("mat_pared.CB");
+		JTextField ot_mat_pa = (JTextField) formBody.getComponentByName("ot_mat_pa.TF");
+
+		if (mat_pared.getSelectedItem().equals("Otro")) {
+			ot_mat_pa.setEnabled(true);
+		} else {
+			ot_mat_pa.setEnabled(false);
+		}
+	}
+
+	private void setOt_mat_teEnabledIfNeeded() {
+		JComboBox mat_techo = (JComboBox) formBody.getComponentByName("mat_techo.CB");
+		JTextField ot_mat_te = (JTextField) formBody.getComponentByName("ot_mat_te.TF");
+
+		if (mat_techo.getSelectedItem().equals("Otro")) {
+			ot_mat_te.setEnabled(true);
+		} else {
+			ot_mat_te.setEnabled(false);
+		}
+	}
+
+	private void setOt_mat_piEnabledIfNeeded() {
+		JComboBox mat_piso = (JComboBox) formBody.getComponentByName("mat_piso.CB");
+		JTextField ot_mat_pi = (JTextField) formBody.getComponentByName("ot_mat_pi.TF");
+
+		if (mat_piso.getSelectedItem().equals("Otro")) {
+			ot_mat_pi.setEnabled(true);
+		} else {
+			ot_mat_pi.setEnabled(false);
+		}
+	}
+
+	private void setCual_chEnabledIfNeeded() {
+		JCheckBox consumo_h = (JCheckBox) formBody.getComponentByName("consumo_h.CHB");
+		JTextField cual_ch = (JTextField) formBody.getComponentByName("cual_ch.TF");
+
+		if (consumo_h.isSelected()) {
+			cual_ch.setEnabled(true);
+		} else {
+			cual_ch.setEnabled(false);
+		}
+	}
+
+	private void setOt_dep_almEnabledIfNeeded() {
+		JCheckBox dep_almac = (JCheckBox) formBody.getComponentByName("dep_almac.CHB");
+		JTextField ot_dep_alm = (JTextField) formBody.getComponentByName("ot_dep_alm.TF");
+
+		if (dep_almac.isSelected()) {
+			ot_dep_alm.setEnabled(true);
+		} else {
+			ot_dep_alm.setEnabled(false);
+		}
+	}
+
 	@Override
 	protected void fillSpecificValues() {
+		setOtras_actEnabledIfNeeded();
+		setOtros_ingEnabledIfNeeded();
+		setOt_stat_viEnabledIfNeeded();
+		setPro_vivsexEnabledIfNeeded();
+		setOt_mat_paEnabledIfNeeded();
+		setOt_mat_teEnabledIfNeeded();
+		setOt_mat_piEnabledIfNeeded();
+		setCual_chEnabledIfNeeded();
+		setOt_dep_almEnabledIfNeeded();
+	}
+
+	protected void setListeners() {
+		super.setListeners();
+		JCheckBox hay_ot_act = (JCheckBox) formBody.getComponentByName("hay_ot_act.CHB");
+		hay_ot_act.setActionCommand("otras_act");
+		hay_ot_act.addActionListener(this);
+		JCheckBox hay_ot_ing = (JCheckBox) formBody.getComponentByName("hay_ot_ing.CHB");
+		hay_ot_ing.setActionCommand("otros_ing");
+		hay_ot_ing.addActionListener(this);
+		JComboBox estatus_vi = (JComboBox) formBody.getComponentByName("estatus_vi.CB");
+		estatus_vi.setActionCommand("ot_stat_vi");
+		estatus_vi.addActionListener(this);
+		JCheckBox pro_viv = (JCheckBox) formBody.getComponentByName("pro_viv.CHB");
+		pro_viv.setActionCommand("pro_vivsex");
+		pro_viv.addActionListener(this);
+		JComboBox mat_piso = (JComboBox) formBody.getComponentByName("mat_piso.CB");
+		mat_piso.setActionCommand("ot_mat_pi");
+		mat_piso.addActionListener(this);
+		JComboBox mat_pared = (JComboBox) formBody.getComponentByName("mat_pared.CB");
+		mat_pared.setActionCommand("ot_mat_pa");
+		mat_pared.addActionListener(this);
+		JComboBox mat_techo = (JComboBox) formBody.getComponentByName("mat_techo.CB");
+		mat_techo.setActionCommand("ot_mat_te");
+		mat_techo.addActionListener(this);
+		JCheckBox consumo_h = (JCheckBox) formBody.getComponentByName("consumo_h.CHB");
+		consumo_h.setActionCommand("cual_ch");
+		consumo_h.addActionListener(this);
+		JCheckBox dep_almac = (JCheckBox) formBody.getComponentByName("dep_almac.CHB");
+		dep_almac.setActionCommand("ot_dep_alm");
+		dep_almac.addActionListener(this);
+	}
+
+	@Override
+	protected void removeListeners() {
+		JCheckBox hay_ot_act = (JCheckBox) formBody.getComponentByName("hay_ot_act.CHB");
+		hay_ot_act.removeActionListener(this);
+		JCheckBox hay_ot_ing = (JCheckBox) formBody.getComponentByName("hay_ot_ing.CHB");
+		hay_ot_ing.removeActionListener(this);
+		JComboBox estatus_vi = (JComboBox) formBody.getComponentByName("estatus_vi.CB");
+		estatus_vi.removeActionListener(this);
+		JCheckBox pro_viv = (JCheckBox) formBody.getComponentByName("pro_viv.CHB");
+		pro_viv.removeActionListener(this);
+		JComboBox mat_piso = (JComboBox) formBody.getComponentByName("mat_piso.CB");
+		mat_piso.removeActionListener(this);
+		JComboBox mat_pared = (JComboBox) formBody.getComponentByName("mat_pared.CB");
+		mat_pared.removeActionListener(this);
+		JComboBox mat_techo = (JComboBox) formBody.getComponentByName("mat_techo.CB");
+		mat_techo.removeActionListener(this);
+		JCheckBox consumo_h = (JCheckBox) formBody.getComponentByName("consumo_h.CHB");
+		consumo_h.removeActionListener(this);
+		JCheckBox dep_almac = (JCheckBox) formBody.getComponentByName("dep_almac.CHB");
+		dep_almac.removeActionListener(this);
+		super.removeListeners();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		super.actionPerformed(e);
+		String action = e.getActionCommand();
+		if (action.equals("otras_act")) {
+			setOtras_actEnabledIfNeeded();
+		} else 
+			if (action.equals("otros_ing")) {
+				setOtros_ingEnabledIfNeeded();
+			} else 
+				if (action.equals("ot_stat_vi")) {
+					setOt_stat_viEnabledIfNeeded();
+				} else 
+					if (action.equals("pro_vivsex")) {
+						setPro_vivsexEnabledIfNeeded();
+					} else 
+						if (action.equals("ot_mat_pi")) {
+							setOt_mat_piEnabledIfNeeded();
+						} else 
+							if (action.equals("ot_mat_pa")) {
+								setOt_mat_paEnabledIfNeeded();
+							} else 
+								if (action.equals("ot_mat_te")) {
+									setOt_mat_teEnabledIfNeeded();
+								} else 
+									if (action.equals("cual_ch")) {
+										setCual_chEnabledIfNeeded();
+									} else 
+										if (action.equals("ot_dep_alm")) {
+											setOt_dep_almEnabledIfNeeded();
+										}
 	}
 }
