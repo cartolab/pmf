dd3361771c0aee557a27ac93f9ecd9c7cbf770d0
Jorge López <jlopez@cartolab.es>
2011-04-04 18:27:39 +0200

    Farm table column names updated.
    Two column names have been made shorter in order
    to fit better in their table.

--- src/es/udc/cartolab/gvsig/pmf/forms/ColumnNamesTranslator.java
+++ src/es/udc/cartolab/gvsig/pmf/forms/ColumnNamesTranslator.java
@@ -11,8 +11,8 @@ public abstract class ColumnNamesTranslator {
 		translations.put("cod_com", "Comunidad");
 		translations.put("tipo_cul", "Cultivo");
 		translations.put("area", "Área cultivada (Mz.)");
-		translations.put("vol_proc", "Volumen producido (Qt.)");
-		translations.put("vol_cons", "Volumen consumido (Qt.)");
+		translations.put("vol_proc", "Vol. producido (Qt.)");
+		translations.put("vol_cons", "Vol. consumido (Qt.)");
 	}
 
 	public static String getLongName(String name) {
