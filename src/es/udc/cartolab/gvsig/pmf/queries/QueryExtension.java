6a03688d9daf344f2399a195ad6b0ec6037c5946
Jorge López <jlopez@cartolab.es>
2011-05-13 12:46:39 +0200

    Queries icon replaced.
    The icon has been replaced with a better one.
    Also made some indentation changes.

--- src/es/udc/cartolab/gvsig/pmf/queries/QueryExtension.java
+++ src/es/udc/cartolab/gvsig/pmf/queries/QueryExtension.java
@@ -9,47 +9,47 @@ import es.udc.cartolab.gvsig.navtableforms.Utils;
 
 public class QueryExtension extends Extension {
 
-	private final String neededLayers[] = { "vivienda", "parcela", "comunidad" };
-	// "areas_protegidas" };
+    private final String neededLayers[] = { "vivienda", "parcela", "comunidad" };
+    // "areas_protegidas" };
 
-	private BaseView view;
+    private BaseView view;
 
-	@Override
-	public void initialize() {
-		PluginServices.getIconTheme().registerDefault(
-				"query-launcher-icon",
-				this.getClass().getClassLoader().getResource(
-						"images/report.png"));
-	}
+    @Override
+    public void initialize() {
+	PluginServices.getIconTheme().registerDefault(
+		"query-launcher-icon",
+		this.getClass().getClassLoader().getResource(
+			"images/queries.png"));
+    }
 
-	@Override
-	public void execute(String actionCommand) {
-		SelectQueryDialog d = new SelectQueryDialog(view);
-		PluginServices.getMDIManager().addWindow(d);
-	}
+    @Override
+    public void execute(String actionCommand) {
+	SelectQueryDialog d = new SelectQueryDialog(view);
+	PluginServices.getMDIManager().addWindow(d);
+    }
 
-	@Override
-	public boolean isEnabled() {
-		IWindow window = PluginServices.getMDIManager().getActiveWindow();
-		boolean isView = false;
-		boolean neededLayersArePresent = true;
-		if (window instanceof BaseView) {
-			isView = true;
-			view = (BaseView) window;
-			for (String layerName : neededLayers) {
-				if (Utils.getFlyrVect(view, layerName) == null) {
-					neededLayersArePresent = false;
-					break;
-				}
-			}
-
+    @Override
+    public boolean isEnabled() {
+	IWindow window = PluginServices.getMDIManager().getActiveWindow();
+	boolean isView = false;
+	boolean neededLayersArePresent = true;
+	if (window instanceof BaseView) {
+	    isView = true;
+	    view = (BaseView) window;
+	    for (String layerName : neededLayers) {
+		if (Utils.getFlyrVect(view, layerName) == null) {
+		    neededLayersArePresent = false;
+		    break;
 		}
-		return isView && neededLayersArePresent;
-	}
+	    }
 
-	@Override
-	public boolean isVisible() {
-		return true;
 	}
+	return isView && neededLayersArePresent;
+    }
+
+    @Override
+    public boolean isVisible() {
+	return true;
+    }
 
 }
