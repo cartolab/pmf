713687d643452dd376c32e8287e7b4b98cf8a69f
Jorge López <jlopez@cartolab.es>
2011-05-13 13:08:50 +0200

    Added coordinates to Comunidad.

--- src/es/udc/cartolab/gvsig/pmf/forms/validation/model/ComunidadModel.java
+++ src/es/udc/cartolab/gvsig/pmf/forms/validation/model/ComunidadModel.java
@@ -14,284 +14,326 @@ import es.udc.cartolab.gvsig.navtableforms.validation.FormModel;
  */
 public class ComunidadModel extends FormModel {
 
-	// Private variables needed for framework
-	// ********************************************
-	private String nombre;
-	private String departa;
-	private String n_habit;
-	private String cod_com;
-	private String municip;
-	private String n_fam;
+    // Private variables needed for framework
+    // ********************************************
+    private String nombre;
+    private String departa;
+    private String n_habit;
+    private String cod_com;
+    private String municip;
+    private String n_fam;
+    private String x;
+    private String y;
+    private String z;
 
-	private boolean cent_jard;
-	private boolean cent_esc;
-	private boolean cent_cc;
-	private boolean cent_div;
-	private boolean csalud;
-	private boolean creunion;
-	private boolean luz_elec;
-	private boolean alcantar;
-	private boolean tfn_fijo;
-	private boolean agua_pot;
+    private boolean cent_jard;
+    private boolean cent_esc;
+    private boolean cent_cc;
+    private boolean cent_div;
+    private boolean csalud;
+    private boolean creunion;
+    private boolean luz_elec;
+    private boolean alcantar;
+    private boolean tfn_fijo;
+    private boolean agua_pot;
 
-	// Constructor and initialization methods
-	// *************************************************************
-	public ComunidadModel(Container c) {
-		super(c);
-	}
+    // Constructor and initialization methods
+    // *************************************************************
+    public ComunidadModel(Container c) {
+	super(c);
+    }
 
-	@Override
-	public String getModelName() {
-		return "ComunidadModel";
-	}
+    @Override
+    public String getModelName() {
+	return "ComunidadModel";
+    }
 
-	@Override
-	protected void setDefaultValues() {
-		setDefaultValuesForIntFields();
-		setDefaultValuesForStringFields();
-		setDefaultValuesForBooleanFields();
-		setDefaultValuesForWidgetMap();
-	}
+    @Override
+    protected void setDefaultValues() {
+	setDefaultValuesForIntFields();
+	setDefaultValuesForStringFields();
+	setDefaultValuesForBooleanFields();
+	setDefaultValuesForWidgetMap();
+    }
 
-	private void setDefaultValuesForWidgetMap() {
-		// int values
-		widgetValues.put("n_habit", n_habit);
-		widgetValues.put("n_fam", n_fam);
+    private void setDefaultValuesForWidgetMap() {
+	// int values
+	widgetValues.put("x", x);
+	widgetValues.put("y", y);
+	widgetValues.put("z", z);
+	widgetValues.put("n_habit", n_habit);
+	widgetValues.put("n_fam", n_fam);
 
-		// string values
-		widgetValues.put("nombre", nombre);
-		widgetValues.put("departa", departa);
-		widgetValues.put("cod_com", cod_com);
-		widgetValues.put("municip", municip);
+	// string values
+	widgetValues.put("nombre", nombre);
+	widgetValues.put("departa", departa);
+	widgetValues.put("cod_com", cod_com);
+	widgetValues.put("municip", municip);
 
-		// boolean values
-		widgetValues.put("cent_jard", String.valueOf(cent_jard));
-		widgetValues.put("cent_esc", String.valueOf(cent_esc));
-		widgetValues.put("cent_cc", String.valueOf(cent_cc));
-		widgetValues.put("cent_div", String.valueOf(cent_div));
-		widgetValues.put("csalud", String.valueOf(csalud));
-		widgetValues.put("creunion", String.valueOf(creunion));
-		widgetValues.put("luz_elec", String.valueOf(luz_elec));
-		widgetValues.put("alcantar", String.valueOf(alcantar));
-		widgetValues.put("tfn_fijo", String.valueOf(tfn_fijo));
-		widgetValues.put("agua_pot", String.valueOf(agua_pot));
-	}
+	// boolean values
+	widgetValues.put("cent_jard", String.valueOf(cent_jard));
+	widgetValues.put("cent_esc", String.valueOf(cent_esc));
+	widgetValues.put("cent_cc", String.valueOf(cent_cc));
+	widgetValues.put("cent_div", String.valueOf(cent_div));
+	widgetValues.put("csalud", String.valueOf(csalud));
+	widgetValues.put("creunion", String.valueOf(creunion));
+	widgetValues.put("luz_elec", String.valueOf(luz_elec));
+	widgetValues.put("alcantar", String.valueOf(alcantar));
+	widgetValues.put("tfn_fijo", String.valueOf(tfn_fijo));
+	widgetValues.put("agua_pot", String.valueOf(agua_pot));
+    }
 
-	private void setDefaultValuesForStringFields() {
-		nombre = getGvsigDefaultString();
-		departa = getGvsigDefaultString();
-		cod_com = getGvsigDefaultString();
-		municip = getGvsigDefaultString();
-	}
+    private void setDefaultValuesForStringFields() {
+	nombre = getGvsigDefaultString();
+	departa = getGvsigDefaultString();
+	cod_com = getGvsigDefaultString();
+	municip = getGvsigDefaultString();
+    }
 
-	private void setDefaultValuesForIntFields() {
-		// int values
-		n_habit = Integer.toString(getGvsigDefaultInt());
-		n_fam = Integer.toString(getGvsigDefaultInt());
-	}
+    private void setDefaultValuesForIntFields() {
+	// int values
+	n_habit = Integer.toString(getGvsigDefaultInt());
+	n_fam = Integer.toString(getGvsigDefaultInt());
+	x = Integer.toString(getGvsigDefaultInt());
+	y = Integer.toString(getGvsigDefaultInt());
+	z = Integer.toString(getGvsigDefaultInt());
+    }
 
-	private void setDefaultValuesForBooleanFields() {
-		cent_jard = getGvsigDefaultBoolean();
-		cent_esc = getGvsigDefaultBoolean();
-		cent_cc = getGvsigDefaultBoolean();
-		cent_div = getGvsigDefaultBoolean();
-		csalud = getGvsigDefaultBoolean();
-		creunion = getGvsigDefaultBoolean();
-		luz_elec = getGvsigDefaultBoolean();
-		alcantar = getGvsigDefaultBoolean();
-		tfn_fijo = getGvsigDefaultBoolean();
-		agua_pot = getGvsigDefaultBoolean();
-	}
+    private void setDefaultValuesForBooleanFields() {
+	cent_jard = getGvsigDefaultBoolean();
+	cent_esc = getGvsigDefaultBoolean();
+	cent_cc = getGvsigDefaultBoolean();
+	cent_div = getGvsigDefaultBoolean();
+	csalud = getGvsigDefaultBoolean();
+	creunion = getGvsigDefaultBoolean();
+	luz_elec = getGvsigDefaultBoolean();
+	alcantar = getGvsigDefaultBoolean();
+	tfn_fijo = getGvsigDefaultBoolean();
+	agua_pot = getGvsigDefaultBoolean();
+    }
 
-	// Getters & Setters
-	// *************************************************************
-	public String getNombre() {
-		return nombre;
-	}
+    // Getters & Setters
+    // *************************************************************
+    public String getX() {
+	return x;
+    }
 
-	public void setNombre(String newValue) {
-		String oldValue = getNombre();
-		nombre = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("NOMBRE"), oldValue, newValue);
-	}
+    public void setX(String newValue) {
+	String oldValue = getX();
+	x = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("X"), oldValue, newValue);
+    }
 
-	public String getDeparta() {
-		return departa;
-	}
+    public String getY() {
+	return y;
+    }
 
-	public void setDeparta(String newValue) {
-		String oldValue = getDeparta();
-		departa = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("DEPARTA"), oldValue, newValue);
-	}
+    public void setY(String newValue) {
+	String oldValue = getY();
+	y = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("Y"), oldValue, newValue);
+    }
 
-	public String getN_habit() {
-		return n_habit;
-	}
+    public String getZ() {
+	return z;
+    }
 
-	public void setN_habit(String newValue) {
-		String oldValue = getN_habit();
-		n_habit = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("N_HABIT"), oldValue, newValue);
-	}
+    public void setZ(String newValue) {
+	String oldValue = getZ();
+	z = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("Z"), oldValue, newValue);
+    }
 
-	public String getCod_com() {
-		return cod_com;
-	}
+    public String getNombre() {
+	return nombre;
+    }
 
-	public void setCod_com(String newValue) {
-		String oldValue = getCod_com();
-		cod_com = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("COD_COM"), oldValue, newValue);
-	}
+    public void setNombre(String newValue) {
+	String oldValue = getNombre();
+	nombre = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("NOMBRE"), oldValue, newValue);
+    }
 
-	public String getMunicip() {
-		return municip;
-	}
+    public String getDeparta() {
+	return departa;
+    }
 
-	public void setMunicip(String newValue) {
-		String oldValue = getMunicip();
-		municip = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("MUNICIP"), oldValue, newValue);
-	}
+    public void setDeparta(String newValue) {
+	String oldValue = getDeparta();
+	departa = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("DEPARTA"), oldValue, newValue);
+    }
 
-	public String getN_fam() {
-		return n_fam;
-	}
+    public String getN_habit() {
+	return n_habit;
+    }
 
-	public void setN_fam(String newValue) {
-		String oldValue = getN_fam();
-		n_fam = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("N_FAM"), oldValue, newValue);
-	}
+    public void setN_habit(String newValue) {
+	String oldValue = getN_habit();
+	n_habit = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("N_HABIT"), oldValue, newValue);
+    }
 
-	public boolean getCent_jard() {
-		return cent_jard;
-	}
+    public String getCod_com() {
+	return cod_com;
+    }
 
-	public void setCent_jard(boolean newValue) {
-		boolean oldValue = getCent_jard();
-		cent_jard = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("CENT_JARD"), oldValue, newValue);
-	}
+    public void setCod_com(String newValue) {
+	String oldValue = getCod_com();
+	cod_com = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("COD_COM"), oldValue, newValue);
+    }
 
-	public boolean getCent_esc() {
-		return cent_esc;
-	}
+    public String getMunicip() {
+	return municip;
+    }
 
-	public void setCent_esc(boolean newValue) {
-		boolean oldValue = getCent_esc();
-		cent_esc = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("CENT_ESC"), oldValue, newValue);
-	}
+    public void setMunicip(String newValue) {
+	String oldValue = getMunicip();
+	municip = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("MUNICIP"), oldValue, newValue);
+    }
 
-	public boolean getCent_cc() {
-		return cent_cc;
-	}
+    public String getN_fam() {
+	return n_fam;
+    }
 
-	public void setCent_cc(boolean newValue) {
-		boolean oldValue = getCent_cc();
-		cent_cc = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("CENT_CC"), oldValue, newValue);
-	}
+    public void setN_fam(String newValue) {
+	String oldValue = getN_fam();
+	n_fam = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("N_FAM"), oldValue, newValue);
+    }
 
-	public boolean getCent_div() {
-		return cent_div;
-	}
+    public boolean getCent_jard() {
+	return cent_jard;
+    }
 
-	public void setCent_div(boolean newValue) {
-		boolean oldValue = getCent_div();
-		cent_div = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("CENT_DIV"), oldValue, newValue);
-	}
+    public void setCent_jard(boolean newValue) {
+	boolean oldValue = getCent_jard();
+	cent_jard = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("CENT_JARD"), oldValue, newValue);
+    }
 
-	public boolean getCsalud() {
-		return csalud;
-	}
+    public boolean getCent_esc() {
+	return cent_esc;
+    }
 
-	public void setCsalud(boolean newValue) {
-		boolean oldValue = getCsalud();
-		csalud = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("CSALUD"), oldValue, newValue);
-	}
+    public void setCent_esc(boolean newValue) {
+	boolean oldValue = getCent_esc();
+	cent_esc = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("CENT_ESC"), oldValue, newValue);
+    }
 
-	public boolean getCreunion() {
-		return creunion;
-	}
+    public boolean getCent_cc() {
+	return cent_cc;
+    }
 
-	public void setCreunion(boolean newValue) {
-		boolean oldValue = getCreunion();
-		creunion = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("CREUNION"), oldValue, newValue);
-	}
+    public void setCent_cc(boolean newValue) {
+	boolean oldValue = getCent_cc();
+	cent_cc = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("CENT_CC"), oldValue, newValue);
+    }
 
-	public boolean getLuz_elec() {
-		return luz_elec;
-	}
+    public boolean getCent_div() {
+	return cent_div;
+    }
 
-	public void setLuz_elec(boolean newValue) {
-		boolean oldValue = getLuz_elec();
-		luz_elec = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("LUZ_ELEC"), oldValue, newValue);
-	}
+    public void setCent_div(boolean newValue) {
+	boolean oldValue = getCent_div();
+	cent_div = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("CENT_DIV"), oldValue, newValue);
+    }
 
-	public boolean getAlcantar() {
-		return alcantar;
-	}
+    public boolean getCsalud() {
+	return csalud;
+    }
 
-	public void setAlcantar(boolean newValue) {
-		boolean oldValue = getAlcantar();
-		alcantar = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("ALCANTAR"), oldValue, newValue);
-	}
+    public void setCsalud(boolean newValue) {
+	boolean oldValue = getCsalud();
+	csalud = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("CSALUD"), oldValue, newValue);
+    }
 
-	public boolean getTfn_fijo() {
-		return tfn_fijo;
-	}
+    public boolean getCreunion() {
+	return creunion;
+    }
 
-	public void setTfn_fijo(boolean newValue) {
-		boolean oldValue = getTfn_fijo();
-		tfn_fijo = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("TFN_FIJO"), oldValue, newValue);
-	}
+    public void setCreunion(boolean newValue) {
+	boolean oldValue = getCreunion();
+	creunion = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("CREUNION"), oldValue, newValue);
+    }
 
-	public boolean getAgua_pot() {
-		return agua_pot;
-	}
+    public boolean getLuz_elec() {
+	return luz_elec;
+    }
 
-	public void setAgua_pot(boolean newValue) {
-		boolean oldValue = getAgua_pot();
-		agua_pot = newValue;
-		firePropertyChange(PROPERTIES_MAP.get("AGUA_POT"), oldValue, newValue);
-	}
+    public void setLuz_elec(boolean newValue) {
+	boolean oldValue = getLuz_elec();
+	luz_elec = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("LUZ_ELEC"), oldValue, newValue);
+    }
 
-	// Maps
-	// **************************************************************************
+    public boolean getAlcantar() {
+	return alcantar;
+    }
 
-	// map with widget values
-	@Override
-	public Map<String, String> getWidgetValues() {
-		// int values
-		widgetValues.put("n_habit", n_habit);
-		widgetValues.put("n_fam", n_fam);
+    public void setAlcantar(boolean newValue) {
+	boolean oldValue = getAlcantar();
+	alcantar = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("ALCANTAR"), oldValue, newValue);
+    }
 
-		// string values
-		widgetValues.put("nombre", nombre);
-		widgetValues.put("departa", departa);
-		widgetValues.put("cod_com", cod_com);
-		widgetValues.put("municip", municip);
+    public boolean getTfn_fijo() {
+	return tfn_fijo;
+    }
 
-		// boolean values
-		widgetValues.put("cent_jard", String.valueOf(cent_jard));
-		widgetValues.put("cent_esc", String.valueOf(cent_esc));
-		widgetValues.put("cent_cc", String.valueOf(cent_cc));
-		widgetValues.put("cent_div", String.valueOf(cent_div));
-		widgetValues.put("csalud", String.valueOf(csalud));
-		widgetValues.put("creunion", String.valueOf(creunion));
-		widgetValues.put("luz_elec", String.valueOf(luz_elec));
-		widgetValues.put("alcantar", String.valueOf(alcantar));
-		widgetValues.put("tfn_fijo", String.valueOf(tfn_fijo));
-		widgetValues.put("agua_pot", String.valueOf(agua_pot));
-		return widgetValues;
-	}
+    public void setTfn_fijo(boolean newValue) {
+	boolean oldValue = getTfn_fijo();
+	tfn_fijo = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("TFN_FIJO"), oldValue, newValue);
+    }
+
+    public boolean getAgua_pot() {
+	return agua_pot;
+    }
+
+    public void setAgua_pot(boolean newValue) {
+	boolean oldValue = getAgua_pot();
+	agua_pot = newValue;
+	firePropertyChange(PROPERTIES_MAP.get("AGUA_POT"), oldValue, newValue);
+    }
+
+    // Maps
+    // **************************************************************************
+
+    // map with widget values
+    @Override
+    public Map<String, String> getWidgetValues() {
+	// int values
+	widgetValues.put("x", x);
+	widgetValues.put("y", y);
+	widgetValues.put("z", z);
+	widgetValues.put("n_habit", n_habit);
+	widgetValues.put("n_fam", n_fam);
+
+	// string values
+	widgetValues.put("nombre", nombre);
+	widgetValues.put("departa", departa);
+	widgetValues.put("cod_com", cod_com);
+	widgetValues.put("municip", municip);
+
+	// boolean values
+	widgetValues.put("cent_jard", String.valueOf(cent_jard));
+	widgetValues.put("cent_esc", String.valueOf(cent_esc));
+	widgetValues.put("cent_cc", String.valueOf(cent_cc));
+	widgetValues.put("cent_div", String.valueOf(cent_div));
+	widgetValues.put("csalud", String.valueOf(csalud));
+	widgetValues.put("creunion", String.valueOf(creunion));
+	widgetValues.put("luz_elec", String.valueOf(luz_elec));
+	widgetValues.put("alcantar", String.valueOf(alcantar));
+	widgetValues.put("tfn_fijo", String.valueOf(tfn_fijo));
+	widgetValues.put("agua_pot", String.valueOf(agua_pot));
+	return widgetValues;
+    }
 }
