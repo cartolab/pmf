da62823250a60e6290dc610a749e56bed365ce0d
Jorge López <jlopez@cartolab.es>
2013-08-23 19:19:27 +0200

    More test data

--- db/sqlite/test_data.sql
+++ db/sqlite/test_data.sql
@@ -24,3 +24,9 @@ INSERT INTO parcelas VALUES (2, '1', 'test1', '2', NULL, NULL, NULL, NULL, NULL,
 
 INSERT INTO presencia_institucional VALUES (1, '1', 'test1', 'test1', 'test1');
 INSERT INTO presencia_institucional VALUES (2, '1', 'test2', 'test2', 'test2');
+
+INSERT INTO limites_parcela VALUES (1, '1', 'test', 'test1', 0, 0, 0, null);
+INSERT INTO limites_parcela VALUES (2, '2', 'test', 'test2', 0, 0, 0, null);
+
+INSERT INTO fuentes_comunitarias VALUES (1, '1', 'test1', 0, 0, 0, null);
+INSERT INTO fuentes_comunitarias VALUES (2, '2', 'test2', 0, 0, 0, null);
