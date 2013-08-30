095fc56c333d7d9924d151c405d365a2b003d526
Jorge López <jlopez@cartolab.es>
2013-08-30 15:52:45 +0200

    Added extension for auto-loading all layers.
    The extensions automatically connects to the
    sqlite file pmf.sqlite, which should be inside
    the cfg gvSIG folder, and loads the ELLE map
    PMF. It also hides all DBConnection and ELLE
    extensions' buttons.

--- src/es/udc/cartolab/gvsig/pmf/utils/PmfConstants.java
+++ src/es/udc/cartolab/gvsig/pmf/utils/PmfConstants.java
@@ -7,5 +7,7 @@ public interface PmfConstants {
     public static final String DATA_SCHEMA = "pmf";
     public static final String GPS_MATCHING_FILES = Launcher.getAppHomeDir()
 	    + "gps-matching-files";
+    public static final String GENERAL_MAP = "PMF";
+    public static final String SQLITE_FILE = "pmf.sqlite";
 
 }
