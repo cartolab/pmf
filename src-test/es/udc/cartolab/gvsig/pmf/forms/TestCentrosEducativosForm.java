5bf092fc25d11e3fce1d540c81beda345c959bc8
Francisco Puga <fpuga@cartolab.es>
2014-04-21 17:41:27 +0200

    Adds tests for forms

---
+++ src-test/es/udc/cartolab/gvsig/pmf/forms/TestCentrosEducativosForm.java
@@ -0,0 +1,18 @@
+package es.udc.cartolab.gvsig.pmf.forms;
+
+import es.icarto.gvsig.navtableforms.CommonMethodsForTestDBForms;
+import es.udc.cartolab.gvsig.pmf.utils.PmfConstants;
+
+public class TestCentrosEducativosForm extends CommonMethodsForTestDBForms {
+
+    @Override
+    protected String getSchema() {
+	return PmfConstants.DATA_SCHEMA;
+    }
+
+    @Override
+    protected String getTableName() {
+	return CentrosEducativosForm.NAME;
+    }
+
+}
---
+++ src-test/es/udc/cartolab/gvsig/pmf/forms/TestCentrosSaludForm.java
@@ -0,0 +1,18 @@
+package es.udc.cartolab.gvsig.pmf.forms;
+
+import es.icarto.gvsig.navtableforms.CommonMethodsForTestDBForms;
+import es.udc.cartolab.gvsig.pmf.utils.PmfConstants;
+
+public class TestCentrosSaludForm extends CommonMethodsForTestDBForms {
+
+    @Override
+    protected String getSchema() {
+	return PmfConstants.DATA_SCHEMA;
+    }
+
+    @Override
+    protected String getTableName() {
+	return CentrosSaludForm.NAME;
+    }
+
+}
